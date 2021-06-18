ff25b3197cf6a837e1705ef534d57881b993a2db
Kilian Braquin-Mitel <kbraquin@indiebaie.com>
2021-06-18 15:05:24 +0200

    test font opti

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
 import "tailwindcss/tailwind.css";
-import { GoogleFonts } from "next-google-fonts";
 import ThemeProvider from "@/providers/ThemeProvider";
 import { AppProps } from "next/app";
 
 const CustomApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ThemeProvider>
-      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&family=Rubik:wght@400;700&display=swap" />
+      {/* <GoogleFonts href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&family=Rubik:wght@400;700&display=swap" /> */}
       <Component {...pageProps} />
     </ThemeProvider>
   );
