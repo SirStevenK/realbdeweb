44f39c5b9caedcf082e70a9e9bd4f06d8952930e
Kilian Braquin-Mitel <kbraquin@indiebaie.com>
2021-06-18 14:36:22 +0200

    upgrade next.js

--- pages/api/auth/user.ts
+++ pages/api/auth/user.ts
@@ -1,13 +1,12 @@
 import GetLoginSession from "@/lib/auth/GetLoginSession";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiHandler } from "next";
 
-export default async (
-  req: NextApiRequest,
-  res: NextApiResponse
-): Promise<void> => {
+const route: NextApiHandler = async (req, res) => {
   const session = await GetLoginSession(req);
   // After getting the session you may want to fetch for the user instead
   // of sending the session's payload directly, this example doesn't have a DB
   // so it won't matter in this case
   res.status(200).json({ user: session || null });
 };
+
+export default route;
